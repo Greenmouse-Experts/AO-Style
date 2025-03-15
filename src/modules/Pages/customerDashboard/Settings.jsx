b99276cec53ffeb2b900cc5031eac4b1f2b39b28
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-03-15 01:56:09 +0100

    Update

--- src/modules/Pages/customerDashboard/Settings.jsx
+++ src/modules/Pages/customerDashboard/Settings.jsx
@@ -13,9 +13,9 @@ const Settings = () => {
                     <Link to="/customer" className="text-blue-500 hover:underline">Dashboard</Link> &gt; Settings
                 </p>
             </div>
-            <div className="flex bg-gray-100 min-h-screen">
+            <div className="flex bg-gray-100">
                 {/* Sidebar */}
-                <div className="w-1/5 bg-white p-4 rounded-lg">
+                <div className="w-1/5 bg-white h-screen p-4 rounded-lg">
                     <ul className="space-y-4 text-gray-600">
                         <li className="font-medium text-purple-600">Profile</li>
                         <li>KYC</li>
@@ -118,15 +118,114 @@ const Settings = () => {
 
                                 {bodyTab === "upperBody" && (
                                     <form className="space-y-4 grid grid-cols-2 gap-4 mt-8">
-                                        <input type="text" placeholder="Neck Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Shoulder Width" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Chest Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Waist Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Sleeve Length" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Bicep Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Wrist Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <input type="text" placeholder="Shirt Length" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg" required />
-                                        <button type="submit" className="mt-4 bg-purple-600 text-white px-6 py-2 rounded-md col-span-2">Update Upper Body</button>
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Neck Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Neck Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Shoulder Width</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Shoulder Width" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Chest Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Chest Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Waist Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Waist Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Sleeve Length</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Sleeve Length" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Bicep Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Bicep Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Wrist Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Wrist Circumference" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+                                        <div >
+                                            <label className="block text-gray-700 mb-4">Shirt Length</label>
+                                            <div className="relative">
+                                                <input type="text" placeholder="Shirt Length" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+
+
+                                        <button type="submit" className="mt-4 bg-gradient text-white px-6 py-2 rounded-md">Update Upper Body</button>
+                                    </form>
+                                )}
+                                {bodyTab === "lowerBody" && (
+                                    <form className="space-y-4 grid grid-cols-2 gap-4 mt-4">
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Waist Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" placeholder="Enter the circumference of your waist" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Hip Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" placeholder="Enter the circumference of your hip" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Thigh Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" placeholder="Enter the circumference of your thigh" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <label className="block text-gray-700 mb-4">Knee Circumference</label>
+                                            <div className="relative">
+                                                <input type="text" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" placeholder="Enter the circumference of your knee" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div className="col-span-2">
+                                            <label className="block text-gray-700 mb-4">Trouser Length (Waist to Ankle)</label>
+                                            <div className="relative">
+                                                <input type="text" className="w-full p-4 border border-[#CCCCCC] outline-none rounded-lg pr-10" placeholder="Enter your trouser length" required />
+                                                <span className="absolute right-3 top-1/2 border-l-1 border-[#CCCCCC] pl-2 transform -translate-y-1/2 text-gray-500">cm</span>
+                                            </div>
+                                        </div>
+                                        <div className="col-span-2 flex justify-between">
+                                            <button className="bg-gray-400 text-white px-6 py-2 rounded-md">Back</button>
+                                            <button type="submit" className="bg-purple-600 text-white px-6 py-2 rounded-md">Update Lower Body</button>
+                                        </div>
                                     </form>
                                 )}
                             </div>

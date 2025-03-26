8110918394de1f73809222bca138e0f315be7724
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-03-26 07:35:05 +0100

    Fabric-Dasboard

--- src/modules/Pages/fabricDashboard/Settings.jsx
+++ src/modules/Pages/fabricDashboard/Settings.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const Settings = () => {
     const [activeTab, setActiveTab] = useState("personalDetails");
     const [activeSection, setActiveSection] = useState("Profile");

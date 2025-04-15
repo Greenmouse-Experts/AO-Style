8d7b5219906898cb1d276e2faccc82a094245402
One Line Of Code at a Time 👨‍💻. <hmztadeleke@gmail.com>
2025-04-15 12:36:08 +0100

    Update

--- src/modules/Home/About.jsx
+++ src/modules/Home/About.jsx
@@ -107,7 +107,7 @@ const AboutUs = () => {
             <section className="px-6 md:px-16 section lg:px-28 bg-grey-50 relative">
                 <div className="flex flex-col-reverse md:flex-row items-center gap-10">
                     <div className="md:w-1/1 md:text-left">
-                        <button className="text-lg font-medium leading-snug px-6 py-2 bg-gradient text-white rounded-md cursor-pointer">
+                        <button className="text-lg font-medium leading-snug px-6 w-full py-2 bg-gradient text-white rounded-md cursor-pointer">
                                 WHO WE ARE
                         </button>
                         <p className="mt-4 text-dark text-base leading-[38px]">
@@ -130,10 +130,29 @@ const AboutUs = () => {
                     </div>
                 </div>
             </section>
-            <section className="px-6 md:px-16 lg:px-28 just bg-[#f7f7f7] relative">
+            <section className="px-6 md:px-16 lg:px-28 section bg-gradient relative">
+                {/* Vision & Mission Cards */}
+                <div className="relative z-10 flex flex-col md:flex-row items-center gap-6">
+                    {sections.map((section, index) => (
+                        <div
+                            key={index}
+                            className="bg-white rounded-lg p-6 md:w-1/2 relative z-10"
+                        >
+                            {/* Icon */}
+                            <div className="bg-[#FFF2FF] p-3 rounded-sm inline-block">
+                                <section.icon className="text-[#E070BB]" size={36} />
+                            </div>
+                            {/* Title & Description */}
+                            <h3 className="text-xl font-medium mt-4">{section.title}</h3>
+                            <p className="text-dark leading-loose mt-2 mb-4">{section.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <section className="px-6 md:px-16 lg:px-28 just bg-[#f7f7f7] section relative">
                 {/* Reviews Badge */}
                 <div className="flex justify-center">
-                    <div className="flex items-center gap-2 border border-[#E070BB] px-4 py-2 rounded-full sm:-mt-6 mt-2 bg-white">
+                    <div className="flex items-center gap-2 border border-[#E070BB] px-4 py-2 rounded-full mt-16 bg-white">
                         {/* Avatar Group */}
                         <div className="flex -space-x-2">
                             <img
@@ -168,33 +187,12 @@ const AboutUs = () => {
                         anyone seeking bespoke clothing.
                     </p>
                 </div>
-
-                {/* Vision & Mission Cards */}
-                <div className="relative z-10 mt-16 flex flex-col md:flex-row items-center gap-6">
-                    {sections.map((section, index) => (
-                        <div
-                            key={index}
-                            className="bg-white rounded-lg p-6 md:w-1/2 relative z-10"
-                        >
-                            {/* Icon */}
-                            <div className="bg-[#FFF2FF] p-3 rounded-sm inline-block">
-                                <section.icon className="text-[#E070BB]" size={36} />
-                            </div>
-                            {/* Title & Description */}
-                            <h3 className="text-xl font-medium mt-4">{section.title}</h3>
-                            <p className="text-dark leading-loose mt-2 mb-4">{section.description}</p>
-                        </div>
-                    ))}
-                </div>
-
-                {/* Blue Background */}
-                <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient z-0"></div>
             </section>
             <div className="Resizer section px-4">
                 {/* Section 1 */}
                 <div className="flex flex-col-reverse md:flex-row items-center gap-10 py-8">
                     <div className="md:w-1/1 md:text-left">
-                    <button className="text-lg font-medium leading-snug px-6 py-2 bg-gradient text-white rounded-md cursor-pointer">
+                    <button className="text-lg font-medium  w-full leading-snug px-6 py-2 bg-gradient text-white rounded-md cursor-pointer">
                                OUR FOCUS
                         </button>
                         <p className="mt-4 text-dark text-base leading-[38px]">
@@ -291,7 +289,7 @@ const AboutUs = () => {
                     </div>
                 </div>
             </section> */}
-            <div className="just"></div>
+            {/* <div className="just"></div> */}
             <ShippingInfo />
         </>
 

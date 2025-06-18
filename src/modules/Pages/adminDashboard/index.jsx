2c0686fdd6fd88e4789c68c191a4d59b404007f3
Rolexero <rotimiolaolu@gmail.com>
2025-06-18 10:24:07 +0100

    modify dashboard ui

--- src/modules/Pages/adminDashboard/index.jsx
+++ src/modules/Pages/adminDashboard/index.jsx
@@ -5,32 +5,32 @@ import IncomeExpensesChart from "./components/IncomeExpensesChart";
 import UserAnalyticsChart from "./components/UserAnalyticsChart";
 import AddedUser from "./components/AddedUser";
 import ProductInventory from "./components/ProductInventory";
+import RecentActivitiesTable from "./components/RecentActivities";
 
 export default function SuperDashboard() {
-    return (
-        <>
-            <Cards />
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
-                <div className="lg:col-span-2 flex flex-col">
-                    <HeaderCard  />
-                    <div className="mt-6">
-                        <IncomeExpensesChart className="flex-grow" />
-                    </div>
-                    <div className="mt-6">
-                        <AddedUser className="flex-grow" />
-                    </div>
-                </div>
-                <div className="lg:col-span-1">
-                    <WalletPage />
-                    <div className="mt-6">
-                        <UserAnalyticsChart className="flex-grow" />
-                    </div>
-                    <div className="mt-6">
-                        <ProductInventory className="flex-grow" />
-                    </div>
-                </div>
-            </div>
-
-        </>
-    );
+  return (
+    <>
+      <Cards />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
+        <div className="lg:col-span-2 flex flex-col">
+          <HeaderCard />
+          <div className="mt-6">
+            <IncomeExpensesChart className="flex-grow" />
+          </div>
+          <div className="mt-6">
+            <RecentActivitiesTable className="flex-grow" />
+          </div>
+        </div>
+        <div className="lg:col-span-1">
+          <WalletPage />
+          <div className="mt-6">
+            <UserAnalyticsChart className="flex-grow" />
+          </div>
+          <div className="mt-6">
+            <ProductInventory className="flex-grow" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

dba79be2e29819466f0b955e8990c33259ce9936
Rolexero <rotimiolaolu@gmail.com>
2025-06-24 16:46:03 +0100

    remove customer flow

--- src/modules/Pages/customerDashboard/Settings.jsx
+++ src/modules/Pages/customerDashboard/Settings.jsx
@@ -98,7 +98,7 @@ const Settings = () => {
         {/* Sidebar */}
         <div className="w-full md:w-1/5 bg-white md:mb-0 mb-6 h-fit p-4 rounded-lg">
           <ul className="space-y-2 text-gray-600">
-            {["Profile", "KYC", "Bank Details", "Security"].map((item) => (
+            {["Profile", "Security"].map((item) => (
               <li
                 key={item}
                 className={`cursor-pointer px-4 py-3 rounded-lg transition-colors duration-300 ${

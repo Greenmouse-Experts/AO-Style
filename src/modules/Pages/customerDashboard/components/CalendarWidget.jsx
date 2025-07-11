23a82c78b5ddb71fe477cb42250bc70591c4a1b8
Prof.T Tech <218093409+greenmousedev0@users.noreply.github.com>
2025-07-11 17:20:16 +0100

    Update

--- src/modules/Pages/customerDashboard/components/CalendarWidget.jsx
+++ src/modules/Pages/customerDashboard/components/CalendarWidget.jsx
@@ -5,7 +5,7 @@ import "react-day-picker/dist/style.css";
 export default function CalendarWidget() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   return (
-    <div className="bg-white p-4 rounded-md">
+    <div className="bg-white lg:flex md:hidden p-4 rounded-md">
       <DayPicker mode="single" selected={selectedDate} onSelect={setSelectedDate} />
     </div>
   );

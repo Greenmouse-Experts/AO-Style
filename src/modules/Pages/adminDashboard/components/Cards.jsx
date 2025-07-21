7cea7df3719450638708269b3ebae35255886a62
Rotimi <rotimiolaolu@gmail.com>
2025-07-21 17:14:30 +0100

    fixed

--- src/modules/Pages/adminDashboard/components/Cards.jsx
+++ src/modules/Pages/adminDashboard/components/Cards.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 
-export default function StatsCard(data) {
+export default function StatsCard({ data }) {
   const { data: cardData } = data;
   const stats = useMemo(
     () => [
@@ -33,19 +33,17 @@ export default function StatsCard(data) {
   );
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-6">
+    <div className="flex flex-wrap gap-3">
       {stats.map((stat, index) => (
         <div
           key={index}
-          className="flex items-center justify-between bg-white p-4 rounded-md"
+          className="flex items-center justify-between bg-white p-4 rounded-md w-full sm:w-[calc(50%-0.75rem)] lg:w-[calc(25%-0.75rem)]"
         >
           <div>
             <p className="text-xl font-bold leading-loose">{stat.value}</p>
             <p className="text-[#8299EA]">{stat.label}</p>
           </div>
-          <div
-            className={`w-20 h-20 flex items-center justify-center rounded-full`}
-          >
+          <div className="w-20 h-20 flex items-center justify-center rounded-full">
             <img src={stat.image} alt={stat.label} className="w-14 h-14" />
           </div>
         </div>

adfb1feab635f65d8958ac52dba5d64bed984e04
Rotimi <rotimiolaolu@gmail.com>
2025-07-21 17:16:46 +0100

    fixed

--- src/modules/Pages/logisticsDashboard/components/Cards.jsx
+++ src/modules/Pages/logisticsDashboard/components/Cards.jsx
@@ -1,42 +1,50 @@
 export default function StatsCard() {
-    const stats = [
-      {
-        image: "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_38685_ycsbd5.png",
-        value: 87,
-        label: "Total Orders",
-      },
-      {
-        image: "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273108/AoStyle/Group_3868e5_iedrv5.png", 
-        value: 17,
-        label: "New Orders",
-      },
-      {
-        image: "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_38686_m4tzu8.png", 
-        value: "3.8",
-        label: "Ratings/Review",
-      },
-      {
-        image: "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_328686_ndt2b0.png", 
-        value: "N200,000",
-        label: "Total Income",
-      },
-    ];
-  
-    return (
-      <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 gap-6">
-        {stats.map((stat, index) => (
-          <div key={index} className="flex items-center justify-between bg-white p-4 rounded-md">
-            <div>
-              <p className="text-xl font-bold leading-loose">{stat.value}</p>
-              <p className="text-[#8299EA]">{stat.label}</p>
-            </div>
-  
-            <div className={`w-20 h-20 flex items-center justify-center rounded-full`}>
-              <img src={stat.image} alt={stat.label} className="w-14 h-14" />
-            </div>
+  const stats = [
+    {
+      image:
+        "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_38685_ycsbd5.png",
+      value: 87,
+      label: "Total Orders",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273108/AoStyle/Group_3868e5_iedrv5.png",
+      value: 17,
+      label: "New Orders",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_38686_m4tzu8.png",
+      value: "3.8",
+      label: "Ratings/Review",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/greenmouse-tech/image/upload/v1742273107/AoStyle/Group_328686_ndt2b0.png",
+      value: "N200,000",
+      label: "Total Income",
+    },
+  ];
+
+  return (
+    <div className="flex flex-wrap gap-4 my-8">
+      {stats.map((stat, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-between bg-white p-4 rounded-md w-full sm:w-[calc(50%-0.75rem)] lg:w-[calc(25%-0.75rem)]"
+        >
+          <div>
+            <p className="text-xl font-bold leading-loose">{stat.value}</p>
+            <p className="text-[#8299EA]">{stat.label}</p>
           </div>
-        ))}
-      </div>
-    );
-  }
-  
+
+          <div
+            className={`w-20 h-20 flex items-center justify-center rounded-full`}
+          >
+            <img src={stat.image} alt={stat.label} className="w-14 h-14" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}

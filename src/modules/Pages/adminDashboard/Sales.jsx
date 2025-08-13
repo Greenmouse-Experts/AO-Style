131a8cb6f99afbdcc9b0652d08e24de9368d4dfc
destiny <desto4q@gmail.com>
2025-08-13 09:21:09 +0100

    sales rep cards remov

--- src/modules/Pages/adminDashboard/Sales.jsx
+++ src/modules/Pages/adminDashboard/Sales.jsx
@@ -6,17 +6,16 @@ import MarketRepCard from "./sales-rep/Cards";
 import MarketRepBarComponent from "./sales-rep/Chart";
 
 export default function SuperDashboard() {
+  // <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
+  //   <div className="lg:col-span-2 flex flex-col">
+  //     {/* <MarketRepBarComponent />*/}
+  //   </div>
+  //   <div className="lg:col-span-1">{/* <NewApplicants />*/}</div>
+  // </div>
   return (
     <>
       <MarketRepCard />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
-        <div className="lg:col-span-2 flex flex-col">
-          <MarketRepBarComponent />
-        </div>
-        <div className="lg:col-span-1">
-          <NewApplicants />
-        </div>
-      </div>
+
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
         <div className="lg:col-span-5 flex flex-col">
           <AddedUser className="flex-grow" />

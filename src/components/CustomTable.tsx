ebb551539bbdb92bebb9a953546d95ddc9eb759f
destiny <desto4q@gmail.com>
2025-08-25 14:02:30 +0100

    table z-index fix

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -26,7 +26,7 @@ export default function CustomTable(props: CustomTableProps) {
 
   return (
     <div data-theme="nord" className="p-2  " id="cus-app">
-      <div className="overflow-x-scroll static">
+      <div className="static">
         <table className="table static">
           {/* head */}
           <thead>
@@ -62,7 +62,7 @@ export default function CustomTable(props: CustomTableProps) {
                     {!props.columns?.find((item) => item.key == "action") && (
                       <>
                         <td className="">
-                          <div className="dropdown dropdown-end">
+                          <div className="dropdown dropdown-end ">
                             <div
                               tabIndex={0}
                               role="button"
@@ -74,7 +74,7 @@ export default function CustomTable(props: CustomTableProps) {
                             </div>
                             <ul
                               tabIndex={0}
-                              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+                              className="dropdown-content menu bg-base-100 rounded-box  w-52 p-2 shadow-sm "
                             >
                               {props.actions?.map((action) => {
                                 return (

d2374f5b3ee81221ef7a4d611cd1fe449efbb74f
destiny <desto4q@gmail.com>
2025-08-28 23:21:13 +0100

    overview to transaction page, table

--- src/modules/Pages/salesDashboard/TransactionPage.jsx
+++ src/modules/Pages/salesDashboard/TransactionPage.jsx
@@ -43,7 +43,7 @@ export default function TransactionPage() {
 
   return (
     <>
-      <Cards vendorSummaryStat={vendorSummaryStat?.data} />
+      {/* <Cards vendorSummaryStat={vendorSummaryStat?.data} />*/}
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6 mb-6">
         <div className="lg:col-span-2">

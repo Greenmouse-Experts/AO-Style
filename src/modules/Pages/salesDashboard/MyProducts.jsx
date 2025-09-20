e474709c885246b314dafa26bc5f0b9bfa79e12f
sodervan <146486331+sodervan@users.noreply.github.com>
2025-09-20 14:29:25 +0100

    Fix JSX syntax in MyProducts component

--- src/modules/Pages/salesDashboard/MyProducts.jsx
+++ src/modules/Pages/salesDashboard/MyProducts.jsx
@@ -725,6 +725,7 @@ const MyProducts = () => {
           onRefresh={refetchProducts}
           isLoading={productsLoading}
           vendorType={vendorType}
+          />
       )}
 
       {/* Filters and Search */}

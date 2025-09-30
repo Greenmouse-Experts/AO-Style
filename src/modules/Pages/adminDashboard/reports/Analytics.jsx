05f38d2d72cc36e582ceea6095aa04d2e1dcbca8
sodervan <egbowonemmanuel99@gmail.com>
2025-09-30 20:20:11 +0100

    done

--- src/modules/Pages/adminDashboard/reports/Analytics.jsx
+++ src/modules/Pages/adminDashboard/reports/Analytics.jsx
@@ -18,7 +18,7 @@ export default function Analytics() {
         </h2>
       </div>
       <div className="mt-4">
-        <Cards data={data?.data?.userCounts} />
+        <Cards data={data?.data?.userCounts} forExtraData={data?.data} />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
         <div className="lg:col-span-2 flex flex-col">

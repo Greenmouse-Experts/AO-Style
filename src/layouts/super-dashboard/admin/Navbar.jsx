486035d4c4d14d5197f6546e1ac585a3aed510fa
sodervan <egbowonemmanuel99@gmail.com>
2025-10-31 16:04:42 +0100

    done

--- src/layouts/super-dashboard/admin/Navbar.jsx
+++ src/layouts/super-dashboard/admin/Navbar.jsx
@@ -87,7 +87,7 @@ export default function Navbar({ toggleSidebar }) {
             )}
 
             {isDropdownOpen && (
-              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-100 shadow-lg rounded-lg z-50">
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-100 shadow-lg rounded-lg z-[9999]">
                 <ul className="py-2">
                   <li className="px-4 py-2 hover:bg-gray-100">
                     <Link

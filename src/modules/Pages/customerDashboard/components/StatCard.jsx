39e1299cfcd58822ee9535a5ff44be5eba49a3f5
sodervan <egbowonemmanuel99@gmail.com>
2025-12-22 11:07:05 +0100

    tailors/vendors

--- src/modules/Pages/customerDashboard/components/StatCard.jsx
+++ src/modules/Pages/customerDashboard/components/StatCard.jsx
@@ -39,7 +39,7 @@ export default function StatsCard() {
                   customerExpensesStat?.data?.styleExpenses
               )
         }`,
-        label: "Total Spent",
+        label: "Fabric/Tailors",
       },
     ],
     [data?.data]
